3011f448052a80ba097d58a9e5dd9066024bf36b
limichange <limichange@hotmail.com>
2022-05-23 13:47:32 +0800

    fix: swiper import lint error (#13)

--- src/components/base/Swiper/Swiper.tsx
+++ src/components/base/Swiper/Swiper.tsx
@@ -1,5 +1,7 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+/* eslint-disable import/no-unresolved */
+
 import 'swiper/css';
 import 'swiper/css/free-mode';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export { Swiper, SwiperSlide };
