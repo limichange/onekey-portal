6387445faf1db3a6981b29c461fb18863a2bb840
limichange <limichange@hotmail.com>
2022-05-23 13:49:06 +0800

    fix: navigation backdrop filter not work (#15)

--- src/components/base/Navigation/NavigationAnimationWrap.tsx
+++ src/components/base/Navigation/NavigationAnimationWrap.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 
+import { detect } from 'detect-browser';
 import {
   motion,
   useSpring,
@@ -16,11 +17,19 @@ export interface NavigationAnimationWrapProps {
 export const NavigationAnimationWrap: FC<NavigationAnimationWrapProps> = (
   props,
 ) => {
+  const browser = detect();
+
   const { paddingRange, isSpring = false, children } = props;
   const { scrollY } = useViewportScroll();
   const paddingValue = useTransform(scrollY, [0, 100], paddingRange);
   const backgroundColor = useTransform(scrollY, (value: number) => {
-    const opacity = value > 90 ? 0.9 : value / 100;
+    let opacity = 0;
+
+    if (browser?.name === 'firefox') {
+      opacity = value >= 100 ? 1 : value / 100;
+    } else {
+      opacity = value > 90 ? 0.9 : value / 100;
+    }
 
     return `rgba(255, 255, 255, ${opacity})`;
   });
@@ -40,6 +49,7 @@ export const NavigationAnimationWrap: FC<NavigationAnimationWrapProps> = (
         paddingTop: isSpring ? paddingSpringValue : paddingValue,
         paddingBottom: isSpring ? paddingSpringValue : paddingValue,
         backdropFilter: isSpring ? blurSpringValue : blur,
+        WebkitBackdropFilter: isSpring ? blurSpringValue : blur,
       }}
     >
       {children}
