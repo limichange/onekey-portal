4f16c9ff11178eddb0444e93552769b91f053c0d
limichange <limichange@hotmail.com>
2022-05-25 16:56:00 +0800

    feat: change the menu position (#30)

--- src/components/base/Navigation/NormalNavigation/NormalNavigation.tsx
+++ src/components/base/Navigation/NormalNavigation/NormalNavigation.tsx
@@ -50,7 +50,7 @@ export const NormalNavigation: React.FC<NormalNavigationProps> = () => {
               },
             }}
           />
-          <Ul xs={{ display: 'flex', gap: 32, alignItems: 'stretch' }}>
+          <Ul xs={{ flex: 1, display: 'flex', gap: 32, paddingLeft: 12 }}>
             {data.map((item) => (
               <Li
                 key={item.name}
@@ -73,9 +73,9 @@ export const NormalNavigation: React.FC<NormalNavigationProps> = () => {
               />
               <NavigationItem>EN</NavigationItem>
             </Box> */}
-
-            <LaunchAppButton variant="outline" />
           </Ul>
+
+          <LaunchAppButton variant="outline" />
         </Container>
       </NavigationAnimationWrap>
 
