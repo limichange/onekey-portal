c7ecaf22a43990474ff23c502181b5bc97d091cd
limichange <limichange@hotmail.com>
2022-06-22 18:09:47 +0800

    feat: test env (#111)

--- src/utils/staticAssetPrefix.ts
+++ src/utils/staticAssetPrefix.ts
@@ -1,6 +1,8 @@
 import pathBrowserify from 'path-browserify';
 
 export function staticAssetPrefix(path: string): string {
+  console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL);
+
   const publicUrl = process.env.PUBLIC_URL || '';
   const newUrl = pathBrowserify.join(publicUrl, path);
 
