af51830fccdd68c4f592f23b4e9d73c3d7942eca
ll__ <loatheb.zhao@gmail.com>
2022-06-22 22:32:25 +0800

    fix: join function name issue (#114)

--- src/utils/staticAssetPrefix.ts
+++ src/utils/staticAssetPrefix.ts
@@ -2,7 +2,7 @@ import urlJoin from 'url-join';
 
 export function staticAssetPrefix(path: string): string {
   const publicUrl = PUBLIC_URL || '';
-  const newUrl = urlJoin.join(publicUrl, path);
+  const newUrl = urlJoin(publicUrl, path);
 
   return newUrl;
 }
