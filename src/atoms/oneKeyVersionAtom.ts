774e36f9e937c63b05bacb1cad880c9e2f1373a1
ll__ <loatheb.zhao@gmail.com>
2022-06-23 12:12:38 +0800

    fix: rename the store key to avoid the cache issue (#115)

--- src/atoms/oneKeyVersionAtom.ts
+++ src/atoms/oneKeyVersionAtom.ts
@@ -4,7 +4,7 @@ import { atomWithStorage } from 'jotai/utils';
 import { OneKeyVersion } from '../types/OneKeyVersion';
 
 export const oneKeyVersionAtom = atomWithStorage<OneKeyVersion | undefined>(
-  'oneKeyVersion',
+  'OneKeyVersion',
   undefined,
 );
 
